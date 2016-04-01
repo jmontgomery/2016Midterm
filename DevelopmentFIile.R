9b79d641d4efe82bc04735dacd6cdd1307489936
Jacob Montgomery <jacob.montg@gmail.com>
2016-04-01 14:17:08 -0500

    Now with simpsons

--- DevelopmentFIile.R
+++ DevelopmentFIile.R
@@ -2,16 +2,25 @@
 
 
 integrateIt<-function(x, y, a, b, rule="Trap"){
+  ## Do the trapezoidal
+  n<-length(x) # number of points
   if(rule=="Trap"){
-    n<-length(x)
-    h<-(b-a)/(2*n)
-    out<-h*(2*sum(x[2:(n-1)])+x[1]+x[n])
-  }  
+    h<-(b-a)/(2*n) # calculate h. Put 2 in the denominator
+    out<-h*(2*sum(y[2:(n-1)])+y[1]+y[n]) # calculate the outcome
+    }
+  if(rule=="Simp"){
+    h<-(b-a)/(n*3) ## calculate h.  Put 3 in the denominator
+    first<-y[1] ## first element
+    last<-y[n] ## second element
+    evens.sum<-sum(y[seq(2, n-1, by=2)]) ## every other elements excludign first and last
+    odds.sum<-sum(y[seq(3, n-2, by=2)]) # the rest
+    out<-h*(first+4*evens.sum + 2*odds.sum + last) #calculate output
+  }
   return(out)  
 }
 
-x<-seq(-5, 5, by=.01)
+tryx<-seq(-5, 4.99, by=.01)
+length(tryx)
+tryy<-dnorm(tryx)
 
-y<-dnorm(x)
-
-integrateIt(x, y, -5, 5, rule="Trap")
+integrateIt(tryx, tryy, -5, 5, rule="Simp")
